7b664fc8e5e742257edb6deca8dede8c3bc45559
aakashmt97 <tiwari.aakashm.5496@gmail.com>
2021-05-24 22:33:10 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 
 function App() {
   return (
-    <div >
+    <div>
+      {/* <div className="header">
         Phone Directory
+      </div>
+      <button> Add </button>
+      <div>
+        <span> Name </span> <br />
+        <span> Phone </span>
+      </div> */}
+      <label htmlFor="name">Name: </label>
+      <input type="text" id="name" placeholder="Type Here" defaultValue="Shiva"></input>
     </div>
   );
 }
