fbd6029a26038837df2bb02c11ffb8afb2fbf922
aakashmt97 <tiwari.aakashm.5496@gmail.com>
2021-05-31 20:33:35 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 
-function App() {
-  return (
-    <div>
-      {/* <div className="header">
-        Phone Directory
-      </div>
-      <button> Add </button>
+import React, { Component } from 'react';
+import Header from "./Header"
+
+// JSX is Case-Sensitive bcoz Javascript id Case-Sensitive
+
+class App extends Component {
+  render() {
+    return (
+
       <div>
-        <span> Name </span> <br />
-        <span> Phone </span>
-      </div> */}
-      <label htmlFor="name">Name: </label>
-      <input type="text" id="name" placeholder="Type Here" defaultValue="Shiva"></input>
-    </div>
-  );
+        <Header />
+        <button> Add </button>
+        <div>
+          <span> Name </span> <br />
+          <span> Phone </span>
+        </div>
+      </div>
+    );
+  }
 }
 
+
 export default App;
+
+
+  // render() {
+  //   return (
+
+  //     /* This is JSX code */
+  //     <div id="module1">
+  //       <p>REACT JS</p>
+  //     </div>
+
+
+  //     /* This is JavaScript code.  React.createElement(element_name, element_properties, children); */
+  //     React.createElement("div", { id: "module" },
+  //       React.createElement("p", null, "ReactJS")
+  //     )
+  //   );
+  // }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = function () {
+    return (
+        <div className="header">  {/* "class" in HTML == "className" in JSX */}
+            Phone Directory
+        </div>
+    )
+}
+
+export default Header
